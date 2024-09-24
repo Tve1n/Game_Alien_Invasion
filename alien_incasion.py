798d5271d8a387e7b1e3943a7f2f533164d8a7fe
Tve1n <timarumak@gmail.com>
2024-09-24 17:14:28 +0500

    add fleet, add end game, add ship_lefts

--- alien_incasion.py
+++ alien_incasion.py
@@ -1,9 +1,13 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     '''Класс для управления ресурсами и поведением игры.'''
@@ -18,15 +22,25 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
+        # Создание экземпляра для хранения игровой статистики.
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         '''Запуск основного цикла игры.'''
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             self._update_screen()
 
     
@@ -79,17 +93,122 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+
+    def _check_bullet_alien_collisions(self):
+        """Обработка коллизий снарядов с пришельцами."""
+        # Удаление снарядов и пришельцев, участвующих в коллизииях.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Уничтожение существующих снаряжов и создание нового флота.
+            self.bullets.empty()
+            self._create_fleet()
+
+
+    def _create_fleet(self):
+        """Создание флота пришельцев."""
+        # Создание пришельца и вычисление количества пришельцев в ряду
+        # Интервал между соседними пришельцами равен ширине пришельца.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        """Определяем количество рядов, помещающихся на экране."""
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - 
+                            (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Создание флота пришельцев.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        
+        self.settings.fleet_direction *= -1
+
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+
+    def _create_alien(self, alien_number, row_number):
+        # Создание пришельца и размещение его в ряду
+            alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+            self.aliens.add(alien)
+
+
+    def _update_aliens(self):
+        """Обновление позиции всех пришельцев во флоте."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+        # Проверка коллизий "пришелец - корабль."
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        
+        # Проверить, добрались ли пришельцы до нижнего края экрана.
+        self._check_aliens_bottom()
+
+
+    def _ship_hit(self):
+        """Обрабатывает столкновение корабля с пришельцем"""
+        if self.stats.ships_left > 0:
+            # Уменьшение ships_left.
+            self.stats.ships_left -= 1
+
+            # Очистка списков пришельцев и снарядов.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Создание нового флота и размещения корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Пауза.
+            sleep(1)
+        else:
+            self.stats.game_active = False
+
+
+    def _check_aliens_bottom(self):
+        """Проверяет, добрались ли пришельцы до нижнего края экрана."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Происходит то же, что при столкновении с кораблем.
+                self._ship_hit()
+                break
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
+        pygame.display.flip()
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
-
-
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -9,11 +9,20 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 200)
+        
+        # Настройки корабля
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Параметры снаряда
-        self.bullet_speed = 1
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+
+        # Настройки пришельцев
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 20
+        # fleet_direction = 1 Это движение вправо; а -1 это влево.
+        self.fleet_direction = 1
