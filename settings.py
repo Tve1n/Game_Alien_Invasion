43eafe6eaa2919000ada8db7eb74aac01b839863
Tve1n <timarumak@gmail.com>
2024-10-05 18:23:28 +0500

    button play

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ class Settings():
 
         # Параметры снаряда
         self.bullet_speed = 1.5
-        self.bullet_width = 300
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 40
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
 
